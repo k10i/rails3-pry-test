9fe862810fd9f93dc57bfbf0ded38abea9e54579
Kenichi Kanai <kenichi.kanai@gmail.com>
2013-01-31 10:29:32 +0900

    initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PryTest::Application.config.secret_token = 'e1a2e48a8a20c84900a1bc52b71a69b55901b3011e1f9807311a1bb62e9a8cccb84a6f67a7d7a84843f83e8dc4a421faf30e3db797591b8f95ef306be59e7a00'
